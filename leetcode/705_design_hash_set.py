1215f9707207e293bc195ac70d17b6d81db0aab3
Rahul <43738448+rsoni8672@users.noreply.github.com>
2023-05-30 11:06:49 +0530

    705_design_hash_set.py

---
+++ leetcode/705_design_hash_set.py
@@ -0,0 +1,24 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.dummy_set = []
+
+    def add(self, key: int) -> None:
+        if len(self.dummy_set) == 0:
+            self.dummy_set.append(key)
+        else:
+            if key > self.dummy_set[-1]:
+                self.dummy_set.append(key)
+                return
+            for index in range(len(self.dummy_set)):
+                if key == self.dummy_set[index]:
+                    return
+                if key < self.dummy_set[index]:
+                    self.dummy_set.insert(index, key)
+                    break
+    def remove(self, key: int) -> None:
+        if key in self.dummy_set:
+            self.dummy_set.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.dummy_set
